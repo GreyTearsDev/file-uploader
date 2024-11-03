b53422d3488c0761e9aa951f191e4517d3b16cfb
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-11-03 18:32:15 +0100

    feat: Add bcryptjs for password hashing and enhance user management in db module
    - Installed bcryptjs for secure password hashing
    - Refactored user creation to hash passwords before storing
    - Added getUserByID and getUserByUsername functions to fetch users by
      ID and username

--- src/db/db.mjs
+++ src/db/db.mjs
@@ -1,12 +1,33 @@
 import { PrismaClient } from "@prisma/client";
-
+import bcryptjs from "bcryptjs";
 const prisma = new PrismaClient();
 
-const newUser = await prisma.user.create({
-  data: {
-    username: "annsnsna",
-    password: "hdf",
-  },
-});
+export const createUser = async (username, password) => {
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { username: username, password: hashedPassword },
+    });
+    return user;
+  } catch (e) {
+    throw Error("error while creating user: ", e);
+  }
+};
 
-console.log(newUser);
+export const getUserByID = async (id) => {
+  try {
+    const user = await prisma.user.findMany({ where: { id: id } });
+    return user[0];
+  } catch (e) {
+    throw Error("error while fetching user: ", e);
+  }
+};
+
+export const getUserByUsername = async (username) => {
+  try {
+    const user = await prisma.user.findMany({ where: { username: username } });
+    return user[0];
+  } catch (e) {
+    throw Error("error while fetching user: ", e);
+  }
+};
