914ea761ff96e11e94b1954ec3df83f67a3796aa
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-11-04 08:24:24 +0100

    refactor: Update user methods and structure db module
    - Renamed user functions to 'create', 'getByID', and 'getByUsername' for streamlined naming
    - Introduced 'db' function to structure and export user methods under a unified 'user' object
    - Improved modularity and maintainability of user-related database functions

--- src/db/db.mjs
+++ src/db/db.mjs
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import bcryptjs from "bcryptjs";
 const prisma = new PrismaClient();
 
-export const createUser = async (username, password) => {
+const create = async (username, password) => {
   try {
     const hashedPassword = await bcryptjs.hash(password, 10);
     const user = await prisma.user.create({
@@ -14,7 +14,7 @@ export const createUser = async (username, password) => {
   }
 };
 
-export const getUserByID = async (id) => {
+const getByID = async (id) => {
   try {
     const user = await prisma.user.findMany({ where: { id: id } });
     return user[0];
@@ -23,11 +23,21 @@ export const getUserByID = async (id) => {
   }
 };
 
-export const getUserByUsername = async (username) => {
+const getByUsername = async (username) => {
   try {
     const user = await prisma.user.findMany({ where: { username: username } });
     return user[0];
   } catch (e) {
     throw Error("error while fetching user: ", e);
   }
+};
+
+export const db = () => {
+  const user = {
+    create,
+    getByID,
+    getByUsername,
+  };
+
+  return { user };
 };
